385482d849fb4fc909bbec753d7d6e18616546fd
Dev-Kiddo <buzzwithprasanth@gmail.com>
2025-10-24 22:27:32 +0530

    server: implemented Handle unCaught Errors

--- server/server.js
+++ server/server.js
@@ -3,6 +3,16 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./config/config.env" });
 import { connectDb } from "./config/db.js";
 
+// Handle Execption Error
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Server is shuting down, due to Uncaught Exception Errors");
+  process.exit(1); //here we directly exit..
+});
+
+// Execption Error
+// console.log(prasanth) => this will throw an reference error bcoz- prasanth is not defined anywhere
+
 const port = process.env.PORT || 4000;
 
 const server = app.listen(process.env.PORT, () => {
@@ -11,8 +21,9 @@ const server = app.listen(process.env.PORT, () => {
   connectDb();
 });
 
+// Handle Promise Rejection
 process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
   console.log("Server is shuting down, due to unhandeled promise rejection");
-  server.close(() => process.exit(1));
+  server.close(() => process.exit(1)); // here we close the server and then exit
 });
