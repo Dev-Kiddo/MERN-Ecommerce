6fdb5234be0a36aff52962f4f5abf9e2220cc5c8
Dev-Kiddo <buzzwithprasanth@gmail.com>
2025-10-24 22:18:15 +0530

    server: implemented unHandledPromise Rejection

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,9 @@
 import mongoose from "mongoose";
 
 export const connectDb = async function () {
-  try {
-    const data = await mongoose.connect(process.env.MONGO_URI);
+  const data = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`${data.connection.host} db connected successfully`);
-  } catch (err) {
-    console.log("Database Err:", err.message);
-  }
+  console.log(`${data.connection.host} db connected successfully`);
 };
 
 // const productSchema = new mongoose.Schema({});
--- server/server.js
+++ server/server.js
@@ -4,8 +4,15 @@ dotenv.config({ path: "./config/config.env" });
 import { connectDb } from "./config/db.js";
 
 const port = process.env.PORT || 4000;
-app.listen(process.env.PORT, () => {
+
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on ${port}`);
 
   connectDb();
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Server is shuting down, due to unhandeled promise rejection");
+  server.close(() => process.exit(1));
 });
