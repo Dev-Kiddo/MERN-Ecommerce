cb53b888f4dbf9c798ff9b14d53797c2c312681d
Dev-Kiddo <buzzwithprasanth@gmail.com>
2025-10-25 07:12:55 +0530

    server: handle cast error implemented

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,11 +1,26 @@
+import HandleError from "../utils/handleError.js";
+
 export default (err, req, res, next) => {
   // console.log("Error Middleware:", err);
 
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
 
+  // {
+  //     "success": false,
+  //     "message": "CastError: Cast to ObjectId failed for value \"68f2462c396ada6f7c9651036\" (type string) at path \"_id\" for model \"product\"\n    at SchemaObjectId.cast
+  // }
+
+  // CAST ERROR - in getSingleProduct - we pass a wrong product id - will get this type of errr
+  if (err.name === "CastError") {
+    const message = `This is invalid resource ${err.path}`;
+
+    err = new HandleError(message, 404);
+  }
+  
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
+    // message: err.stack,
   });
 };
