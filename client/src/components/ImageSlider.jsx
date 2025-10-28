35709eea452894b2335059e02ce1adb10be989d7
Dev-Kiddo <buzzwithprasanth@gmail.com>
2025-10-28 18:33:39 +0530

    client: product slider implemented

---
+++ client/src/components/ImageSlider.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+const ImageSlider = () => {
+  return (
+    <>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={30}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000 }}
+        loop={true}
+        className="rounded-2xl shadow-md"
+      >
+        <SwiperSlide>
+          <img src="./images/hbanner.png" alt="slide 1" className="w-full h-auto" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img src="./images/hbanner.png" alt="slide 2" className="w-full h-auto" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img src="./images/hbanner.png" alt="slide 3" className="w-full h-auto" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img src="./images/hbanner.png" alt="slide 3" className="w-full h-auto" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default ImageSlider;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ImageSlider from "../components/ImageSlider";
 
 const Home = () => {
   return (
     <section>
       <div className="w-full max-w-7xl mx-auto p-4 md:py-8">
-        <h2 className="text-5xl text-gray-900 text-center">Trending Now</h2>
+        <h2 className="text-5xl text-gray-900 text-center mb-6">Trending Now</h2>
+        <ImageSlider />
       </div>
     </section>
   );
