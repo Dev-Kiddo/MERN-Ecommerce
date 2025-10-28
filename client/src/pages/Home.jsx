c52d8c21484d9a706fa96afb156ff2a90b1403bb
Dev-Kiddo <buzzwithprasanth@gmail.com>
2025-10-28 18:35:21 +0530

    fix product slider

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <section>
       <div className="w-full max-w-7xl mx-auto p-4 md:py-8">
-        <h2 className="text-5xl text-gray-900 text-center mb-6">Trending Now</h2>
         <ImageSlider />
+        <h2 className="text-5xl text-gray-900 text-center mb-6">Trending Now</h2>
       </div>
     </section>
   );
