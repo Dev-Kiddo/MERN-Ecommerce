8f836e519d407fcd396d6c14714b87c9dca01b07
Dev-Kiddo <buzzwithprasanth@gmail.com>
2025-11-17 16:06:36 +0530

    added client url in server to fix cors

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 const isProduction = process.env.NODE_ENV === "production";
-const allowedOrigins = isProduction ? ["https://yourfrontenddomain.com"] : ["http://localhost:5173"];
+const allowedOrigins = isProduction ? ["https://mernecommerce-beige.vercel.app/"] : ["http://localhost:5173"];
 
 // Middlewares
 app.use(express.json());
