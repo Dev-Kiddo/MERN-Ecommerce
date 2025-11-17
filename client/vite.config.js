605f6b460a232c9e7371c7613d155880005fa62f
Dev-Kiddo <buzzwithprasanth@gmail.com>
2025-11-17 18:55:01 +0530

    fix: vite config updated, while deployment warnings popup'd

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,17 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          redux: ["@reduxjs/toolkit", "react-redux"],
+          utils: ["lodash", "dayjs"],
+        },
+      },
+    },
+  },
 });
 
 //   server: {
